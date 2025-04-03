307051e2e391a952049a9d85eb7515bae17564ed
richlaver <rich.laver@gmail.com>
2025-04-03 15:11:32 +0800

    Deleted debugging output

--- setup.py
+++ setup.py
@@ -104,12 +104,6 @@ def points_exist():
     collection_info = client.get_collection(qdrant_info['collection_name'])
     point_count = collection_info.points_count
 
-    st.write('collection_info: ')
-    st.write(collection_info)
-
-    st.write('point_count:')
-    st.write(point_count)
-
     return point_count is not None and point_count > 0
 
 
